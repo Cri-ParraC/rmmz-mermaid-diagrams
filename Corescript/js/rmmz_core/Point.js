99680791c09eb1d80b09837d3a536f89be3dfe1f
Cristian Parra <cri.parrac@gmail.com>
2026-03-27 21:42:09 -0300

    Add splited Corescript v1.10.0

---
+++ Corescript/js/rmmz_core/Point.js
@@ -0,0 +1,23 @@
+//=============================================================================
+// rmmz_core.js v1.10.0
+//=============================================================================
+
+//-----------------------------------------------------------------------------
+/**
+ * The point class.
+ *
+ * @class
+ * @extends PIXI.Point
+ * @param {number} x - The x coordinate.
+ * @param {number} y - The y coordinate.
+ */
+function Point() {
+    this.initialize(...arguments);
+}
+
+Point.prototype = Object.create(PIXI.Point.prototype);
+Point.prototype.constructor = Point;
+
+Point.prototype.initialize = function(x, y) {
+    PIXI.Point.call(this, x, y);
+};
